0bc230bf6c9a510c5e31adff237cce8f900c0923
Sam Verstocken <sam.verstocken@ugent.be>
2014-11-27 16:59:33 +0100

    Fixed what previous commit didn't quite

--- makeHTML.sh
+++ makeHTML.sh
@@ -4,52 +4,84 @@
 # Execute this script with "git" as default directory
 #
 
-#####################################################
-### Search for the most recent version of Qt first ##
-#####################################################
+##############################
+## On the Mac OS X platform ##
+##############################
 
-if [ -f $HOME/Qt5.3.2/5.3/clang_64/bin/qhelpgenerator ]
+if [ "$(uname)" == "Darwin" ]
 then
-  # On the Mac OS X platform
-  if [ "$(uname)" == "Darwin" ]
+
+  #####################################################
+  ### Search for the most recent version of Qt first ##
+  #####################################################
+
+  if [ -f $HOME/Qt5.3.2/5.3/clang_64/bin/qhelpgenerator ]
   then
+    
     # generate html documentation in a temporary folder next to the git folder
     /Applications/Doxygen.app/Contents/Resources/doxygen doc/html.doxygen     
   
-  # On the Linux platform
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+    # move Qt compressed help file
+    mkdir -p ../doc
+    mv -f ../html/skirt.qch ../doc/SKIRT.qch
+    
+  ############################################################
+  ## Search for the older Qt version that is also supported ##
+  ############################################################
+    
+  elif [ -f $HOME/Qt5.2.1/5.2.1/clang_64/bin/qhelpgenerator ]
   then
+      
+    # generate html documentation in a temporary folder next to the git folder
+    /Applications/Doxygen.app/Contents/Resources/doxygen doc/html_qt521.doxygen
+    
+    # move Qt compressed help file
+    mkdir -p ../doc
+    mv -f ../html/skirt.qch ../doc/SKIRT.qch
+    
+  else
+    echo "Error: could not find the Qt help file generator. If Qt is installed in a custom location, change this file accordingly."	
+  fi
+    
+##########################
+# On the Linux platform ##
+##########################
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then
+    
+  #####################################################
+  ### Search for the most recent version of Qt first ##
+  #####################################################
+    
+  if [ -f $HOME/Qt5.3.2/5.3/gcc_64/bin/qhelpgenerator ]
+  then
+        
     # generate html documentation in a temporary folder next to the git folder
     doxygen doc/html_ubuntu.doxygen
-  fi
 
-  # move Qt compressed help file
-  mkdir -p ../doc
-  mv -f ../html/skirt.qch ../doc/SKIRT.qch
+    # move Qt compressed help file
+    mkdir -p ../doc
+    mv -f ../html/skirt.qch ../doc/SKIRT.qch
 
-#############################################################################################
-## Search for the older Qt version that is also supported (and a lot of SKIRT users have)  ##
-#############################################################################################
+  ############################################################
+  ## Search for the older Qt version that is also supported ##
+  ############################################################
 
-elif [ -f $HOME/Qt5.2.1/5.2.1/clang_64/bin/qhelpgenerator ]
-then
-  # On the Mac OS X platform
-  if [ "$(uname)" == "Darwin" ]
+  elif [ -f $HOME/Qt5.2.1/5.2.1/gcc_64/bin/qhelpgenerator ]
   then
-    # generate the html documentation in a temporary folder next to the git folder
-    /Applications/Doxygen.app/Contents/Resources/doxygen doc/html_qt521.doxygen
   
-  # On the Linux platform
-  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
-  then
     # generate html documentation in a temporary folder next to the git folder
     doxygen doc/html_qt521_ubuntu.doxygen
+
+    # move Qt compressed help file
+    mkdir -p ../doc
+    mv -f ../html/skirt.qch ../doc/SKIRT.qch
+  
+  else
+    echo "Error: could not find the Qt help file generator. If Qt is installed in a custom location, change this file accordingly."	
   fi
 
-  # move Qt compressed help file
-  mkdir -p ../doc
-  mv -f ../html/skirt.qch ../doc/SKIRT.qch
-
 else
-  echo "Error: could not find the Qt help file generator. If Qt is installed in a custom location, change this file accordingly."	
+  echo "Platforms other than Mac OS X or Ubuntu are not supported!"
 fi
