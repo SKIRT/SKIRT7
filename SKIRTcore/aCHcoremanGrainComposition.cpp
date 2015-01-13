8139daeb837defa512949f0f49e7c3b904de09d5
Sam Verstocken <sam.verstocken@ugent.be>
2015-01-13 14:19:58 +0100

    Setup the base class GrainComposition before aCHcoremanGrainComposition

--- SKIRTcore/aCHcoremanGrainComposition.cpp
+++ SKIRTcore/aCHcoremanGrainComposition.cpp
@@ -15,6 +15,8 @@ aCHcoremanGrainComposition::aCHcoremanGrainComposition()
 
 void aCHcoremanGrainComposition::setupSelfBefore()
 {
+    GrainComposition::setupSelfBefore();
+
     setBulkDensity(3.33e3);
     loadLogHeatCapacityGrid("GrainComposition/DustEM/hcap/C_aCH_coreman_d20.DAT");
     loadOpticalGrid(true, "GrainComposition/Min/aCH_coreman_d20_Jones2013_SKIRT.dat", false, false, false, false);
