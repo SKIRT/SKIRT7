486e5932331dcf23c098e688e7a3d079feeb52d9
Peter Camps <peter.camps@ugent.be>
2015-01-20 16:26:57 +0100

    add documentation concerning support for polarization

--- SKIRTcore/FullInstrument.hpp
+++ SKIRTcore/FullInstrument.hpp
@@ -12,7 +12,19 @@
 ////////////////////////////////////////////////////////////////////
 
 /** A FullInstrument object represents an advanced instrument that records individual contributions
-    to the flux from various sources. Rather than the total flux \f$F_\lambda^{\text{tot}}\f$, this
+    to the flux from various sources, including the components of the Stokes vector for polarized
+    radiation. For each type of flux contribution being recorded, a FullInstrument object holds two
+    array data members: a simple 1D array (the F-array) that stores the integrated flux at every
+    wavelength index, and a 3D array (the f-array) corresponding to the surface brightness in every
+    pixel, at every wavelength index. The instrument dynamically adapts to the characteristics of
+    the simulation it is part of. For example, if there is no dust system, only the direct stellar
+    flux is recorded (since there is nothing else). If dust emission is turned off in the dust
+    system, the thermal radiation emitted by dust is not recorded (since it will be zero).
+    Similarly, if none of the dust mixtures in the dust sytem support polarization, the components
+    of the Stokes vector are not recorded. These adjustments save memory and processing time where
+    appropriate.
+
+    Generally speaking, rather than the total flux \f$F_\lambda^{\text{tot}}\f$, this
     instrument separately stores the contributions due to direct stellar radiation
     (\f$F_\lambda^{\text{dir}}\f$), due to scattered stellar radiation
     (\f$F_\lambda^{\text{sca}}\f$), and due to thermal radiation emitted by the dust
@@ -20,12 +32,11 @@
     F_\lambda^{\text{dir}} + F_\lambda^{\text{sca}} + F_\lambda^{\text{dus}} \f] The instrument
     also stores the flux from the object as it would be seen without any dust attenuation, i.e. the
     transparent view of the system. Furthermore, the contributions from individual scattering
-    levels (up to a certain maximum level \f$N_{\text{max}}\f$) are recorded separately. By
-    default, the maximum level is set to zero, effectively disabling this feature. Internally, the
-    class contains \f$N_{\text{max}}+4\f$ sets of two vectors each as data members: a simple 1D
-    vector (the F-vector) that stores the integrated flux at every wavelength index, and a 3D
-    vector (the f-vector) corresponding to the surface brightness in every pixel, at every
-    wavelength index. */
+    levels (up to a certain maximum level \f$N_{\text{max}}\f$) can be recorded separately. By
+    default, the maximum level is set to zero, effectively disabling this feature. Finally, the
+    instrument also records the values corresponding to each of the elements of the Stokes vector,
+    i.e. \f$Q_\lambda^{\text{tot}}\f$, \f$U_\lambda^{\text{tot}}\f$, and
+    \f$V_\lambda^{\text{tot}}\f$. Note that these Stokes "flux" values can be negative. */
 class FullInstrument : public SingleFrameInstrument
 {
     Q_OBJECT
@@ -75,27 +86,31 @@ protected:
         luminosity in the correct bin of both the 1D F-vectors and the 3D f-vectors. */
     void detect(PhotonPackage* pp);
 
-    /** This function calibrates and outputs the instrument data.
-        The calibration takes care of the conversion from bolometric luminosity units to
-        flux density units (for the F-vector) and surface brightness units (for the f-vector). The
-        function creates five FITS files called <tt>prefix_instrument_total.fits</tt>,
-        <tt>prefix_instrument_direct.fits</tt>, <tt>prefix_instrument_scattered.fits</tt>,
-        <tt>prefix_instrument_dust.fits</tt> and <tt>prefix_instrument_transparent.fits</tt> that
-        contain the 3D vectors with the surface brightness \f$\lambda f_\lambda^{\text{tot}} = \nu
-        f_\nu^{\text{tot}} \f$ etc. in every pixel (if there is only one wavelength, 2D FITS files
-        are created). The unit in which the surface brightness is written depends on the global
-        units choice, but typically it is in \f$\text{W}\,\text{m}^{-2}\,\text{arcsec}^{-2}\f$. The
-        function also creates \f$N_{\text{max}}\f$ separate FITS files
-        <tt>prefix_instrument_scatteringlevel1.fits</tt>, etc. that contain the 3D vectors with the
+    /** This function calibrates and outputs the instrument data. The calibration takes care of the
+        conversion from bolometric luminosity units to flux density units (for the F-vector) and
+        surface brightness units (for the f-vector). Depending on the characteristics of the
+        simulation in which the instrument resides (see description in the class header), this
+        function creates a number of FITS files named <tt>prefix_instr_total.fits</tt>,
+        <tt>prefix_instr_direct.fits</tt>, <tt>prefix_instr_scattered.fits</tt>,
+        <tt>prefix_instr_dust.fits</tt>, <tt>prefix_instr_transparent.fits</tt>,
+        <tt>prefix_instr_stokesQ.fits</tt>, <tt>prefix_instr_stokesU.fits</tt>, and
+        <tt>prefix_instr_stokesV.fits</tt> that contain the frames with the surface brightness in
+        every pixel. Note that the Stokes vector components may have negative values. If there is
+        only one wavelength, the FITS files are 2D; otherwise the FITS files are 3D. The function
+        also creates \f$N_{\text{max}}\f$ separate FITS files named
+        <tt>prefix_instr_scatteringlevel1.fits</tt>, etc. that contain the frames with the
         contribution of the individual scattering levels to the surface brightness. Finally, an
-        ASCII file <tt>prefix_instrument_sed.dat</tt> is created that contains
-        \f$6+N_{\text{max}}\f$ columns. The first six columns contain the wavelength \f$\lambda\f$,
-        the total flux \f$\lambda F_\lambda^{\text{tot}}\f$, the direct flux \f$\lambda
-        F_\lambda^{\text{dir}}\f$, the scattered flux \f$\lambda F_\lambda^{\text{sca}}\f$, the
-        thermal dust flux \f$\lambda F_\lambda^{\text{dus}}\f$ and the transparent flux \f$\lambda
-        F_\lambda^{\text{tra}}\f$. The last \f$N_{\text{max}}\f$ columns contain the contribution
-        of the different scattering levels to the total flux. Typical units for these quantities
-        are \f$\text{W}\,\text{m}^{-2}\f$. */
+        ASCII file <tt>prefix_instrument_sed.dat</tt> is created that contains at least six
+        columns, listing the wavelength \f$\lambda\f$ and the total flux, the direct flux, the
+        scattered flux, the thermal dust flux, and the transparent flux for that wavelength. These
+        six columns are always present, even if the corresponding value is trivially zero. If the
+        simulation supports polarization, the subsequent columns list the three Q, U, V components
+        of the Stokes vector (which may be negative). The last \f$N_{\text{max}}\f$ columns, if
+        any, contain the contribution of the different scattering levels to the total flux. The
+        units in which the surface brightness and the flux densities are written depends on the
+        simulation's choices for flux output style (Neutral indicates \f$\lambda F_\lambda = \nu
+        F_\nu\f$; Wavelength indicates \f$F_\lambda\f$; Frequency indicates \f$F_\nu\f$) and
+        actual units. */
     void write();
 
     //======================== Data Members ========================
