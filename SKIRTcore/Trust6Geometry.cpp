2a189c8adb5e1c537ebe2c317f20a458fcaa9dfc
Peter Camps <peter.camps@ugent.be>
2015-03-04 16:21:08 +0100

    update TRUST6 geometry to benchmark version 0.3.0

--- SKIRTcore/Trust6Geometry.cpp
+++ SKIRTcore/Trust6Geometry.cpp
@@ -6,11 +6,63 @@
 #include <cmath>
 #include "FatalError.hpp"
 #include "Random.hpp"
-#include "SpecialFunctions.hpp"
 #include "Trust6Geometry.hpp"
-#include "Units.hpp"
 
 using namespace std;
+
+//////////////////////////////////////////////////////////////////////
+
+namespace
+{
+    const double AU = 1.49597871e11;    // 1 Astronomical Unit in m
+    const double GCM3 = 1000;           // 1 g/cm3 in kg/m3
+
+    // radial limits
+    const double RMAX1 =  400 *AU;
+    const double RMAX2 =  400 *AU;
+    const double RMAX3 =  100 *AU;
+
+    // vertical limits
+    const double ZMIN1 =  -90 *AU;
+    const double ZMAX1 =  230 *AU;
+    const double ZMIN2 = -230 *AU;
+    const double ZMAX2 = -220 *AU;
+    const double ZMIN3 =  -80 *AU;
+    const double ZMAX3 =  -70 *AU;
+    const double ZCTR3 =  -75 *AU;
+
+    // densities
+    const double RHO1 =  1e-21 *GCM3;
+    const double RHO2 =  1e-18 *GCM3;
+    const double RHOA3 = 8e-15 *GCM3;
+    const double RHOB3 = 4e-18 *GCM3;
+
+    // sigma of gaussian
+    const double SIGMA3 = sqrt(3.)/2. *AU;
+
+    // 2*sigma^2 in denominator of gaussian
+    const double TWO_SIGMA3_SQUARE = 1.5 *AU*AU;
+
+    // equivalent width of gaussian disk, calculated as int_{-5}^{+5} [exp(-2/3 z^2) dz]
+    const double Z_WIDTH3_GAUSS = 2.17080374682060324 *AU;
+
+    // masses
+    const double M1 = M_PI*RMAX1*RMAX1 * (ZMAX1-ZMIN1) * RHO1;
+    const double M2 = M_PI*RMAX2*RMAX2 * (ZMAX2-ZMIN2) * RHO2;
+    const double M3 = M_PI*RMAX3*RMAX3 * ( Z_WIDTH3_GAUSS * RHOA3 + (ZMAX3-ZMIN3) * RHOB3 );
+    const double M = M1 + M2 + M3;
+
+    // mass fractions
+    const double MF1 = M1 / M;
+    const double MF2 = M2 / M;
+    //const double MF3 = M3 / M;
+
+    // normalized densities
+    const double NRHO1 =  RHO1 / M;
+    const double NRHO2 =  RHO2 / M;
+    const double NRHOA3 = RHOA3 / M;
+    const double NRHOB3 = RHOB3 / M;
+}
 
 //////////////////////////////////////////////////////////////////////
 
@@ -20,103 +72,57 @@ Trust6Geometry::Trust6Geometry()
 
 //////////////////////////////////////////////////////////////////////
 
-void Trust6Geometry::setupSelfBefore()
+double Trust6Geometry::density(double R, double z) const
 {
-    AxGeometry::setupSelfBefore();
-
-    // set the values for the main parameters
-
-    _Rmaxv.resize(3,0.0);
-    _zminv.resize(3,0.0);
-    _zmaxv.resize(3,0.0);
-    _rhov.resize(3,0.0);
-    _Mv.resize(3,0.0);
-
-    double AU = Units::AU();
-    _Rmaxv[0] = 400.0*AU;
-    _zminv[0] = -90.0*AU;
-    _zmaxv[0] = 230.0*AU;
-    _rhov[0] = 1e-21;
-    _Rmaxv[1] = 400.0*AU;
-    _zminv[1] = -230.0*AU;
-    _zmaxv[1] = -220.0*AU;
-    _rhov[1] = 1e-18;
-    _Rmaxv[2] = 100.0*AU;
-    _zminv[2] = -80.0*AU;
-    _zmaxv[2] = -70.0*AU;
-    _rhov[2] = 1e-15;
-
-    // determine the proper density normalizations factor
-
-    for (int i=0; i<3; i++)
-        _Mv[i] = _rhov[i] * M_PI * _Rmaxv[i] * _Rmaxv[i] * (_zmaxv[i]-_zminv[i]);
-    double M = _Mv[0] + _Mv[1] + _Mv[2];
-    for (int i=0; i<3; i++)
-    {
-        _rhov[i] /= M;
-        _Mv[i] /= M;
-    }
-}
-
-//////////////////////////////////////////////////////////////////////
-
-double
-Trust6Geometry::density(double R, double z)
-const
-{
-    double rho = 0.0;
-    for (int i=0; i<3; i++)
-        if (R<_Rmaxv[i] && z>_zminv[i] && z<_zmaxv[i]) rho += _rhov[i];
+    double rho = 0;
+    if (R<RMAX1 && z>ZMIN1 && z<ZMAX1) rho += NRHO1;
+    if (R<RMAX2 && z>ZMIN2 && z<ZMAX2) rho += NRHO2;
+    if (R<RMAX3 && z>ZMIN3 && z<ZMAX3) rho += NRHOA3*exp(-(z-ZCTR3)*(z-ZCTR3)/TWO_SIGMA3_SQUARE) + NRHOB3;
     return rho;
 }
 
 //////////////////////////////////////////////////////////////////////
 
-Position
-Trust6Geometry::generatePosition()
-const
+Position Trust6Geometry::generatePosition() const
 {
-    // determine the component from with the position is generated
-
-    double X = _random->uniform();
-    int i = 0;
-    if (X<_Mv[0])
-        i = 0;
-    else if (X<_Mv[0]+_Mv[1])
-        i = 1;
-    else
-        i = 2;
-
-    // determine the actual position
-
-    double R = _Rmaxv[i] * sqrt(_random->uniform());
+    // determine a random azimuth, independent of the component from which the position is generated
     double phi = 2.0*M_PI*_random->uniform();
-    double z = _zminv[i] + _random->uniform()*(_zmaxv[i]-_zminv[i]);
+    double R,z;
+
+    // select a component and generate a position within that component
+    double X = _random->uniform();
+    if (X < MF1)
+    {
+        R = RMAX1 * sqrt(_random->uniform());
+        z = ZMIN1 + _random->uniform()*(ZMAX1-ZMIN1);
+    }
+    else if (X < MF1+MF2)
+    {
+        R = RMAX2 * sqrt(_random->uniform());
+        z = ZMIN2 + _random->uniform()*(ZMAX2-ZMIN2);
+    }
+    else
+    {
+        R = RMAX3 * sqrt(_random->uniform());
+        z = ZMIN3 + _random->gauss()*SIGMA3;
+    }
+
+    // return the position
     return Position(R, phi, z, Position::CYLINDRICAL);
 }
 
 //////////////////////////////////////////////////////////////////////
 
-double
-Trust6Geometry::SigmaR()
-const
+double Trust6Geometry::SigmaR() const
 {
-    double Sigma = 0.0;
-    for (int i=0; i<3; i++)
-        if (_zminv[i]<0.0 && _zmaxv[i]>0.0) Sigma += _rhov[i]*_Rmaxv[i];
-    return Sigma;
+    return RMAX1*NRHO1;
 }
 
 //////////////////////////////////////////////////////////////////////
 
-double
-Trust6Geometry::SigmaZ()
-const
+double Trust6Geometry::SigmaZ() const
 {
-    double Sigma = 0.0;
-    for (int i=0; i<3; i++)
-        Sigma += _rhov[i] * (_zmaxv[i]-_zminv[i]);
-    return Sigma;
+    return (ZMAX1-ZMIN1)*NRHO1 + (ZMAX2-ZMIN2)*NRHO2 + Z_WIDTH3_GAUSS*NRHOA3 + (ZMAX3-ZMIN3) * NRHOB3;
 }
 
 //////////////////////////////////////////////////////////////////////
--- SKIRTcore/Trust6Geometry.hpp
+++ SKIRTcore/Trust6Geometry.hpp
@@ -12,12 +12,36 @@
 ////////////////////////////////////////////////////////////////////
 
 /** The Trust6Geometry class is a subclass of the AxGeometry class and describes the geometry of a
-    lighthouse scenario. It is to be used as one of the benchmark
-    models for the suite of TRUST 3D benchmark models. The geometry consists of three cylindrical
-    components with a constant density. Each of the three components is characterized by a cylindrical radius
+    lighthouse scenario, which is one of the models in the suite of TRUST 3D benchmarks. The
+    geometry consists of three cylindrical components, each characterized by a cylindrical radius
     \f$R_{\text{max},i}\f$, a minimum and maximum height \f$z_{\text{min},i}\f$ and
-    \f$z_{\text{max},i}\f$, and a density \f$\rho_i\f$. The values for these parameters are hard-coded,
-    so there are no free parameters in the model. */
+    \f$z_{\text{max},i}\f$, and a density distribution \f$\rho_i(R,z)\f$. The first two cylinders
+    have a constant density. The third cylinder has a Gaussian profile in the vertical
+    direction, and is also constant in the radial direction. So within the radial and vertical
+    limits of each cylinder, the density distributions are given by:
+    \f[ \rho_1(R,z) = \rho_1 \f]
+    \f[ \rho_2(R,z) = \rho_2 \f]
+    \f[ \rho_3(R,z) = \rho_{\text{A},3} \exp\left(-\frac{(z-z_{\text{ctr},3})^2}{2\sigma_3^2}\right)
+                    + \rho_{\text{B},3} \f]
+    where \f$z_{\text{ctr},3}=(z_{\text{min},3}-z_{\text{min},3})/2\f$.
+
+    The values for all parameters are hard-coded as listed in the table below, so there are no
+    free parameters in the model.
+
+    | Parameter | Value
+    |-----------|-------
+    | \f$R_{\text{max},1}\f$ | \f$400\,\text{AU}\f$
+    | \f$R_{\text{max},2}\f$ | \f$400\,\text{AU}\f$
+    | \f$R_{\text{max},3}\f$ | \f$100\,\text{AU}\f$
+    | \f$z_{\text{min},1};\;z_{\text{max},1}\f$ | \f$-90\,\text{AU};\; 230\,\text{AU}\f$
+    | \f$z_{\text{min},2};\;z_{\text{max},2}\f$ | \f$-230\,\text{AU};\; -220\,\text{AU}\f$
+    | \f$z_{\text{min},3};\;z_{\text{max},3}\f$ | \f$-80\,\text{AU};\; -70\,\text{AU}\f$
+    | \f$\rho_1\f$ | \f$10^{-21}\,\text{g/cm}^3\f$
+    | \f$\rho_2\f$ | \f$10^{-18}\,\text{g/cm}^3\f$
+    | \f$\rho_{\text{A},3};\;\rho_{\text{B},3}\f$ | \f$8\cdot10^{-15}\,\text{g/cm}^3;\; 4\cdot10^{-18}\,\text{g/cm}^3\f$
+    | \f$\sigma_3\f$ | \f$\frac{\sqrt{3}}{2}\,\text{AU}\f$
+
+*/
 class Trust6Geometry : public AxGeometry
 {
     Q_OBJECT
@@ -28,11 +52,6 @@ class Trust6Geometry : public AxGeometry
 public:
     /** The default constructor. */
     Q_INVOKABLE Trust6Geometry();
-
-protected:
-    /** This function just sets the values of the parameters, and normalizes the density of the three
-        components so that the total mass equals one. */
-    void setupSelfBefore();
 
     //======================== Other Functions =======================
 
@@ -52,21 +71,15 @@ public:
     /** This function returns the radial surface density, i.e. the integration of
         the density along a line in the equatorial plane starting at the centre of the coordinate
         system, \f[ \Sigma_R = \int_0^\infty \rho(R,0)\,{\text{d}}R. \f] For the TRUST6 geometry,
-        \f[ \Sigma_R = \sum_i \rho_i\, R_{\text{max},i} \f] where the sum includes the components with
-        \f$z_{\text{min},i} < 0 < z_{\text{max},i}\f$. */
+        \f$ \Sigma_R = \rho_1\, R_{\text{max},1} \f$ since the second and third cylinder do not overlap
+        the equatorial plane. */
     double SigmaR() const;
 
     /** This function returns the Z-axis surface density, i.e. the integration of
         the density along the entire Z-axis, \f[ \Sigma_Z = \int_{-\infty}^\infty \rho(0,0,z)\,
-        {\text{d}}z. \f] For the TRUST6 geometry, \f[ \Sigma_R = \sum_i \rho_i\, (z_{\text{max},i} -
-        z_{\text{min},i}) \f] where the sum includes all components. */
+        {\text{d}}z. \f] For the TRUST6 geometry, the function just sums the contribution of each
+        component. */
     double SigmaZ() const;
-
-    //======================== Data Members ========================
-
-private:
-    // data members initialized during setup
-    std::vector<double> _Rmaxv, _zminv, _zmaxv, _rhov, _Mv;
 };
 
 ////////////////////////////////////////////////////////////////////
