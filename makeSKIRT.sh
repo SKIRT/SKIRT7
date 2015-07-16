09dc70d47c879109a7719851ceaa5757b3b3a251
Maarten Baes <maarten.baes@ugent.be>
2015-07-16 16:26:49 +0200

    makeSKIRT.sh restored

--- makeSKIRT.sh
+++ makeSKIRT.sh
@@ -14,10 +14,10 @@
 # --------------------------------------------------------------------
 
 # List of possible paths of qmake (separate paths with a space)
-PATHLIST=( /Applications/Qt/5.3/clang_64/bin/qmake $HOME/Qt5.4.1/5.4/clang_64/bin/qmake $HOME/Qt5.4.1/5.4/gcc_64/bin/qmake $HOME/Qt/Desktop/5.4.1/bin/qmake /usr/local/Qt/5.4.1/bin/qmake
-           $HOME/Qt5.4.0/5.4/clang_64/bin/qmake $HOME/Qt5.4.0/5.4/gcc_64/bin/qmake $HOME/Qt/Desktop/5.4.0/bin/qmake /usr/local/Qt/5.4.0/bin/qmake
-           $HOME/Qt5.3.2/5.3/clang_64/bin/qmake $HOME/Qt5.3.2/5.3/gcc_64/bin/qmake $HOME/Qt/Desktop/5.3.2/bin/qmake /usr/local/Qt/5.3.2/bin/qmake
-           $HOME/Qt5.2.1/5.2.1/clang_64/bin/qmake $HOME/Qt5.2.1/5.2.1/gcc_64/bin/qmake $HOME/Qt/Desktop/5.2.1/bin/qmake /usr/local/Qt/5.2.1/bin/qmake )
+PATHLIST=( $HOME/Qt5.4.1/5.4/clang_64/bin/qmake $HOME/Qt5.4.1/5.4/gcc_64/bin/qmake $HOME/Qt/Desktop/5.4.1/bin/qmake /usr/local/Qt/5.4.1/bin/qmake
+$HOME/Qt5.4.0/5.4/clang_64/bin/qmake $HOME/Qt5.4.0/5.4/gcc_64/bin/qmake $HOME/Qt/Desktop/5.4.0/bin/qmake /usr/local/Qt/5.4.0/bin/qmake
+$HOME/Qt5.3.2/5.3/clang_64/bin/qmake $HOME/Qt5.3.2/5.3/gcc_64/bin/qmake $HOME/Qt/Desktop/5.3.2/bin/qmake /usr/local/Qt/5.3.2/bin/qmake
+$HOME/Qt5.2.1/5.2.1/clang_64/bin/qmake $HOME/Qt5.2.1/5.2.1/gcc_64/bin/qmake $HOME/Qt/Desktop/5.2.1/bin/qmake /usr/local/Qt/5.2.1/bin/qmake )
 
 QMAKEPATH=""
 
@@ -25,26 +25,26 @@ QMAKEPATH=""
 count=$((${#PATHLIST[@]} - 1))
 for i in $(eval echo {0..$count})
 do
-    POSSIBLEPATH=${PATHLIST[$i]}
-    if [ -e "$POSSIBLEPATH" ]
-    then
-        QMAKEPATH=$POSSIBLEPATH
-        echo "Found qmake in $QMAKEPATH"
-        break  # Break the for loop
-    fi
+POSSIBLEPATH=${PATHLIST[$i]}
+if [ -e "$POSSIBLEPATH" ]
+then
+QMAKEPATH=$POSSIBLEPATH
+echo "Found qmake in $QMAKEPATH"
+break  # Break the for loop
+fi
 done
 
 # If qmake is not found above, try qmake installed in the standard system path
 if [ "$QMAKEPATH" == "" ] && which qmake >/dev/null
 then
-    QMAKEPATH=qmake
+QMAKEPATH=qmake
 fi
 
 # Exit with an error if we don't find qmake
 if [ "$QMAKEPATH" == "" ]
 then
-    echo error: qmake not found
-    exit
+echo error: qmake not found
+exit
 fi
 
 # Create the make file and perform the build
