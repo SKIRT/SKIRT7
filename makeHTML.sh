88f10e177eecccbd22259dd9992592bd1cd69b9d
Peter Camps <peter.camps@ugent.be>
2015-10-06 15:36:47 +0200

    trivial update in makeHTML.sh comnents

--- makeHTML.sh
+++ makeHTML.sh
@@ -6,7 +6,7 @@
 
 # This function returns the Qt version associated with a certain qhelpgenerator executable
 function qt_version {
-    
+
     local VERSION_OUTPUT="$($1 -v | tr -d ' ')"
     local SPLITTED="$(sed s/'(Qt'/' '/g <<< $VERSION_OUTPUT)"
     local LIST=($SPLITTED)
@@ -28,7 +28,7 @@ then
 # On the Linux platform
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
 then
-    
+
   # Generate html documentation in a temporary folder next to the git folder
   doxygen doc/html_ubuntu.doxygen
 
@@ -38,7 +38,7 @@ else
   exit
 fi
 
-  
+
 # Copy the 'mouse over' SKIRT logo
 cp doc/images/SkirtLogoSmall-home.png ../html/SkirtLogoSmall-home.png
 
@@ -47,31 +47,31 @@ python doc/enable_qch_mathjax.py
 
 # Obtain the MathJax repository if it is not yet present
 if [ ! -d ../html/mathjax ]; then
-      
+
   # Clone the repository and checkout version 2.4
   git clone git://github.com/mathjax/MathJax.git ../html/mathjax
   git -C ../html/mathjax checkout -b v2.4-latest origin/v2.4-latest
-    
+
   # Remove unnecessary files and folders
   xargs -I fname rm -r fname < doc/mathjax_delete.txt
-    
+
 fi
 
-# Search for qmake in the home directory
+# Search for qhelpgenerator in the home directory
 PATHLIST="$(find $HOME/Qt* -name qhelpgenerator -type f | tr '\n' ' ')"
 
-# Search for qmake in the $PATH
+# Search for qhelpgenerator in the $PATH
 PATHLIST="$(which qhelpgenerator) $PATHLIST"
 
-# Set the QMAKEPATH to an empty string initially
+# Set the QHELPPATH to an empty string initially
 QHELPPATH=""
 
 # Loop over all the qhelpgenerator paths
 for path in $PATHLIST; do
-    
+
   # Get the associated Qt version
   VERSION="$(qt_version $path)"
-      
+
   # Check whether the Qt version is supported
   if [[ $VERSION > '5.2.0' ]]
   then
@@ -84,21 +84,21 @@ for path in $PATHLIST; do
         then
             QHELPPATH=$path
         fi
-      
+
       # Else, just use this qhelpgenerator
       else
-        QHELPPATH=$path  
+        QHELPPATH=$path
       fi
-  fi 
-      
+  fi
+
 done
-  
+
 # Exit with an error if we don't find a supported qhelpgenerator
 if [ "$QHELPPATH" == "" ]
 then
   echo error: Could not find a qhelpgenerator executable associated with a supported Qt version
   exit
-else  
+else
   echo Using qhelpgenerator in $QHELPPATH
   # Generate the Qt compressed help file
   $QHELPPATH ../html/index_mathjax.qhp -o ../doc/SKIRT.qch
