8d831357a437a3f413391b19a75e4ace5bdefa24
Christian Peest <christian.peest@gmx.de>
2015-11-04 10:37:31 +0100

    Simplify peel-off orientation calculation

--- SKIRTcore/DustMix.cpp
+++ SKIRTcore/DustMix.cpp
@@ -573,13 +573,10 @@ namespace
     // It returns a zero angle when the peel-off scattering event is completely forward or backward,
     // because then the scattering plane is ill-defined; and when the peel-off scattering plane is parallel to
     // the instrument plane (which means there can't be any detection).
-    double angleBetweenScatteringAndInstrumentReference(Direction kc, Direction kn, Direction kx, Direction ky)
+    double angleBetweenScatteringAndInstrumentReference(Direction kn, Direction ky, Direction knew)
     {
-        Vec nobs = Vec::cross(kx,ky);
-        Vec ks = Vec::cross(Vec::cross(kc,kn), nobs);
-        ks /= ks.norm();
-        double cosalpha = Vec::dot(ks,kx);
-        double sinalpha = Vec::dot(Vec::cross(ks,kx), nobs);
+        double cosalpha = Vec::dot(kn,ky);
+        double sinalpha = Vec::dot(Vec::cross(kn,ky), knew);
         double alpha = atan2(sinalpha,cosalpha);
         if (isfinite(alpha)) return alpha;
         return 0;
@@ -644,7 +641,7 @@ void DustMix::scatteringPeelOffPolarization(StokesVector* out, const PhotonPacka
 
         // rotate over the angle between the reference axis in the peel-off scattering plane
         // and the x-axis in the instrument frame
-        double alpha = angleBetweenScatteringAndInstrumentReference(pp->direction(), bfknew, bfkx, bfky);
+        double alpha = angleBetweenScatteringAndInstrumentReference(out->normal(), bfky, bfknew);
         if (alpha) out->rotateStokes(alpha, pp->direction());
     }
 }
