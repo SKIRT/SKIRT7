214785a944cb0eb34a0a154c78b8dd1b11b5bb30
Peter Camps <peter.camps@ugent.be>
2016-09-19 12:21:49 +0200

    Add synthetic circular polarization option to electron dust mix

--- SKIRTcore/ElectronDustMix.hpp
+++ SKIRTcore/ElectronDustMix.hpp
@@ -21,6 +21,11 @@ class ElectronDustMix : public DustMix
     Q_OBJECT
     Q_CLASSINFO("Title", "a 'dust mix' representing a population of electrons")
 
+    Q_CLASSINFO("Property", "circularPolarization")
+    Q_CLASSINFO("Title", "use a synthetic particle that adds circular polarization during scattering")
+    Q_CLASSINFO("Default", "no")
+    Q_CLASSINFO("Silent", "true")
+
     //============= Construction - Setup - Destruction =============
 
 public:
@@ -31,6 +36,24 @@ protected:
     /** This function directly calculates all electron dust mix properties on the simulation's
         wavelength grid. It then adds a single dust population to the dust mix. */
     void setupSelfBefore();
+
+    //======== Setters & Getters for Discoverable Attributes =======
+
+public:
+    /** Sets the flag that indicates whether to use a population of synthetic particles that add
+        circular polarization during scattering; the default value is false, indicating a
+        population of regular electrons. */
+    Q_INVOKABLE void setCircularPolarization(bool value);
+
+    /** Returns the flag that indicates whether to use a population of synthetic particles that add
+        circular polarization. */
+    Q_INVOKABLE bool circularPolarization() const;
+
+    //============= Data Members =============
+
+private:
+    bool _circularPolarization;     // true for synthetic particle that adds circular polarization;
+                                    // false for regular electron
 };
 
 ////////////////////////////////////////////////////////////////////
